fedc32a1a24e527e9029198f1f99b69d985fb104
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-05-25 18:04:31 -0400

    feat(SYNCTHING): optional syncthing service

--- lib/docker.sh
+++ lib/docker.sh
@@ -4,17 +4,52 @@
 
 set -eo pipefail
 
+_is_service_selected() {
+  # $1: The service to check for selection
+
+  local SERVICE
+
+  for SERVICE in "${RPI_SERVICES[@]}"; do
+    if [[ "${SERVICE}" == "${1}" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
 _docker_compose_command() {
   # $@: The command to pass to docker compose
   # set _SERVICE_REMOVE_CONTAINERS to 1 to ensure containers are removed
 
+  local SERVICE
+  SELECTED_SERVICES=()
+
+  for SERVICE in "${RPI_SERVICES[@]}"; do
+    SELECTED_SERVICES+=("--profile")
+    SELECTED_SERVICES+=("${SERVICE}")
+  done
   pushd "services" >> /dev/null
 
-  docker compose "$@"
+  docker compose "${SELECTED_SERVICES[@]}" "$@"
 
   if [[ "${_SERVICE_REMOVE_CONTAINERS}" == "1" ]]; then
-    docker compose rm -f
+    docker compose "${SELECTED_SERVICES[@]}" rm -f
   fi
+
+  popd >> /dev/null
+}
+
+_docker_compose_exec() {
+  # $@: The command to pass to docker compose run
+
+  local SERVICE="${1}"
+
+  shift
+
+  pushd "services" >> /dev/null
+
+  docker compose exec "${SERVICE}" "$@"
 
   popd >> /dev/null
 }
