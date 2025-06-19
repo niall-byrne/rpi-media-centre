36aacca249806cd457b91e19594ab068a73d1a4f
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-06-19 14:59:10 -0400

    test(BASH): add testing environment

---
+++ lib/debug.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# pictl debug library
+
+set -Eeo pipefail
+
+error_handler() {
+  local COMMAND="${BASH_COMMAND}"
+  local EXIT_CODE="$?"
+  local SCRIPT_FILE="${BASH_SOURCE[1]}"
+  local LINE_NUMBER="${BASH_LINENO[0]}"
+
+  echo "**ERROR** source file: ${SCRIPT_FILE} -- line: ${LINE_NUMBER} -- command: ${COMMAND} -- exit code: ${EXIT_CODE}"
+  exit "${EXIT_CODE}"
+}
