b660f5aab59112671f75ab8581d22547b66622f2
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-06-19 21:58:41 -0400

    feat(CLI): add configuration backup command

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -14,6 +14,29 @@ _check_permissions() {
   fi
 }
 
+_configure_backup() {
+  # 1: Backup Source
+  # 2: Backup Target
+
+  if [[ -z "${RPI_PATH_BACKUP}" ]]; then
+    echo "Please configure a value for RPI_PATH_BACKUP in the .rpi configuration file."
+    return 127
+  fi
+
+  echo "Backing up ${2} ..."
+  mkdir -p "${RPI_PATH_BACKUP}/${2}"
+  echo "  ${1} -> ${RPI_PATH_BACKUP}/${2} ..."
+  if [[ -f ${1} ]]; then
+    sudo rsync -ah "${1}" "${RPI_PATH_BACKUP}/${2}"
+  else
+    sudo rsync -ah --delete "${1}/" "${RPI_PATH_BACKUP}/${2}"
+  fi
+
+  chmod 700 "${RPI_PATH_BACKUP}/${2}"
+
+  echo "Backup of ${2} is complete!"
+}
+
 _configure_pictl() {
   if [[ -f .rpi ]]; then
     echo "-- loading .rpi file ... --"
