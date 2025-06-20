321eda22fee270c5dc4eaa05848f39041b249dab
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-06-19 20:13:03 -0400

    refactor(CLI): extract libraries

---
+++ lib/disk.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# pictl disk library
+
+set -eo pipefail
+
+_is_disk_encrypted() {
+  test -f .disk
+}
+
+_is_disk_mounted() {
+  mountpoint "${RPI_MOUNT_POINT}" >> /dev/null 2>&1
+}
+
+_disk_initialize_mounts() {
+  mkdir -p "${RPI_MOUNT_POINT}"/plex/{config,transcode}
+  mkdir -p "${RPI_MOUNT_POINT}"/samba/{cache,lib}
+  mkdir -p "${RPI_MOUNT_POINT}"/shared/{media,transfer}
+}
+
+_disk_lock() {
+  if _is_disk_encrypted; then
+    if _is_disk_mounted; then
+      echo "Unmounting partition ..."
+      sudo umount /dev/mapper/media_centre
+
+      echo "Sealing disk ..."
+      sudo cryptsetup close /dev/mapper/media_centre
+    fi
+  fi
+}
+
+_disk_read_uuid() {
+  tr -d "\n\r" < .disk
+}
+
+_disk_unlock() {
+  if _is_disk_encrypted; then
+    if ! _is_disk_mounted; then
+      ENCRYPTED_DISK_UUID="$(_disk_read_uuid)"
+
+      echo "Decrypting disk ..."
+      sudo cryptsetup luksOpen "/dev/disk/by-uuid/${ENCRYPTED_DISK_UUID}" media_centre
+
+      echo "Checking data ..."
+      sudo fsck /dev/mapper/media_centre
+
+      echo "Mounting partition ..."
+      sudo mount /dev/mapper/media_centre "${RPI_MOUNT_POINT}" -o noatime,rw,errors=remount-ro
+    fi
+  fi
+}
---
+++ lib/io.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# pictl io library
+
+set -eo pipefail
+
+_service_prompt() {
+  # 1: the prompt to display
+  # 2: the variable name to save
+  # 3: optionally set to "password" to suppress output
+
+  local FLAGS="-rp"
+
+  if [[ "${3}" == "password" ]]; then
+    FLAGS="-rsp"
+  fi
+
+  while [[ -z "${!2}" ]]; do
+    # shellcheck disable=SC2229,SC2162
+    read "${FLAGS}" "$1" "$2"
+    if [[ "${3}" == "password" ]]; then
+      echo
+    fi
+  done
+}
