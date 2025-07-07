a158ff9b5a052d3c0d1820a7db66f60977c1c28b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-07-07 11:35:22 -0400

    feat(CLI): add manifest edit command

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -4,16 +4,53 @@
 
 set -eo pipefail
 
-_configure_pictl() {
+_configuration_pictl() {
+  local RPI_CONFIGURATION_QUIET_LOAD=0
+
+  _configuration_pictl_secure_load source .rpi/config
+}
+
+_configuration_pictl_check() {
+  local RPI_CONFIGURATION_QUIET_LOAD=1
+
+  _configuration_pictl_secure_load env -i bash -c "
+  source .rpi/config &&
+  declare -p | \
+      grep '^declare -. RPI_' |
+      sed 's/^declare -. //g' |
+      sed 's/=.*//g' |
+      sort
+"
+}
+
+_configuration_pictl_help() {
+  echo "The config file is a sourced BASH script that configures one or more of the following:"
+
+  # Generate a summary from README.md
+  grep '^| `RPI_' README.md |
+    cut -d "|" -f2,3 |
+    tr -d '`' |
+    sed 's/  */ /g' |
+    awk -F"|" '{ $1 = sprintf("%-40s", $1); print $1 $2}' |
+    sed 's/the \[documentation\](\(.*\)) for/\1 for/g' |
+    sort
+
+  echo "Please see https://github.com/niall-byrne/rpi-media-centre for further details."
+}
+
+_configuration_pictl_secure_load() {
+  # $@: the commands to execute after loading the configuration
+
   if [[ -f .rpi/config ]]; then
-    echo "-- loading .rpi/config file ... --"
+    if [[ "${RPI_CONFIGURATION_QUIET_LOAD}" -ne "1" ]]; then
+      echo "-- loading .rpi/config file ... --"
+    fi
     _filesystem_check_permissions .rpi/config "600"
-    # shellcheck source=/dev/null
-    source .rpi/config
+    "$@"
   fi
 }
 
-_configure_samba() {
+_configuration_samba() {
   if [[ -f .rpi/samba.yml ]]; then
     echo "-- loading .rpi/samba.yml file ... --"
     _filesystem_check_permissions .rpi/samba.yml "600"
@@ -27,18 +64,12 @@ _configure_samba() {
   _io_prompt "Enter Samba Network CIDR: " "RPI_SAMBA_SUBNET"
 }
 
-_configure_syncthing() {
-  _service_syncthing_configure_setting RPI_SYNCTHING_CREDENTIALS_USERNAME gui user
-  _service_syncthing_configure_setting RPI_SYNCTHING_CREDENTIALS_PASSWORD gui password
+_configuration_syncthing() {
+  _configuration_syncthing_setting RPI_SYNCTHING_CREDENTIALS_USERNAME gui user
+  _configuration_syncthing_setting RPI_SYNCTHING_CREDENTIALS_PASSWORD gui password
 }
 
-_service_syncthing_wait() {
-  while ! curl -fkLsS -m 2 127.0.0.1:8384/rest/noauth/health >> /dev/null 2>&1; do
-    sleep 1
-  done
-}
-
-_service_syncthing_configure_setting() {
+_configuration_syncthing_setting() {
   # $1 the optional value to use
   # $@ the config key to set
 
@@ -48,7 +79,11 @@ _service_syncthing_configure_setting() {
 
   if [[ -n "${!VALUE}" ]]; then
     echo "Configuring syncthing '${*}' with environment variable '${VALUE}' ..."
-    _service_syncthing_wait
+
+    while ! curl -fkLsS -m 2 127.0.0.1:8384/rest/noauth/health >> /dev/null 2>&1; do
+      sleep 1
+    done
+
     _docker_compose_exec syncthing syncthing cli config "$@" set "${!VALUE}" >> /dev/null 2>&1
     sleep 1
   fi
