2dcadfb649a16f07f42ed03310f00a42a2ea23b5
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-09-27 14:32:57 -0400

    feat(CONFIGURATION): support syncthing 2.0 cli

--- lib/tests/configuration/test_configuration_syncthing.sh
+++ lib/tests/configuration/test_configuration_syncthing.sh
@@ -1,19 +1,82 @@
 #!/bin/bash
 
 setup() {
-  _mock.create _configuration_syncthing_setting
+  _mock.create _cli_log_warning
+  _mock.create _configuration_syncthing_healthcheck
+  _mock.create _docker_compose_exec
+  _mock.create docker
+  _mock.create _cli_log_success
 }
 
-test_configuration_syncthing__calls_setting_for_username() {
-  _configuration_syncthing
+@parametrize_with_credentials() {
+  # $1: the test function to parametrize
 
-  _configuration_syncthing_setting.mock.assert_any_call_is \
-    "1(RPI_SYNCTHING_CREDENTIALS_USERNAME) 2(gui) 3(user)"
+  @parametrize \
+    "${1}" \
+    "TEST_USERNAME;TEST_PASSWORD;TEST_UID;TEST_GID" \
+    "user1;user1;password1;1001;2001" \
+    "user2;user2;password2;1101;2101"
 }
 
-test_configuration_syncthing__calls_setting_for_password() {
+test_configuration_syncthing__logs_warning_message() {
   _configuration_syncthing
 
-  _configuration_syncthing_setting.mock.assert_any_call_is \
-    "1(RPI_SYNCTHING_CREDENTIALS_PASSWORD) 2(gui) 3(password)"
+  _cli_log_warning.mock.assert_called_once_with \
+    "1(Configuring syncthing service credentials...)"
+}
+
+test_configuration_syncthing__calls_syncthing_healthcheck_twice() {
+  _configuration_syncthing
+
+  _configuration_syncthing_healthcheck.mock.assert_calls_are \
+    "" \
+    ""
+}
+
+# shellcheck disable=SC2034
+test_configuration_syncthing__@vary__calls_docker_compose_exec_as_expected() {
+  local RPI_SVC_UID="${TEST_UID}"
+  local RPI_SVC_GID="${TEST_GID}"
+  local RPI_SYNCTHING_CREDENTIALS_PASSWORD="${TEST_PASSWORD}"
+  local RPI_SYNCTHING_CREDENTIALS_USERNAME="${TEST_USERNAME}"
+
+  _configuration_syncthing
+
+  _docker_compose_exec.mock.assert_calls_are \
+    "1(syncthing) 2(syncthing) 3(generate) 4(--gui-password=${TEST_PASSWORD}) 5(--gui-user=${TEST_USERNAME})" \
+    "1(syncthing) 2(chown) 3(${TEST_UID}:${TEST_GID}) 4(/config/config.xml)"
+}
+
+@parametrize_with_credentials \
+  test_configuration_syncthing__@vary__calls_docker_compose_exec_as_expected
+
+# shellcheck disable=SC2034
+test_configuration_syncthing__restarts_the_syncthing_service() {
+  _configuration_syncthing
+
+  docker.mock.assert_called_once_with \
+    "1(restart) 2(syncthing)"
+}
+
+test_configuration_syncthing__logs_success_message() {
+  _configuration_syncthing
+
+  _cli_log_success.mock.assert_called_once_with \
+    "1(Configuration complete!)"
+}
+
+# shellcheck disable=SC2034
+test_configuration_syncthing__calls_dependencies_in_the_correct_sequence() {
+  _mock.sequence.record.start
+
+  _configuration_syncthing
+
+  _mock.sequence.assert_is \
+    "_cli_log_warning" \
+    "_configuration_syncthing_healthcheck" \
+    "_docker_compose_exec" \
+    "_docker_compose_exec" \
+    "docker" \
+    "_configuration_syncthing_healthcheck" \
+    "_cli_log_success"
 }
