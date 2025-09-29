6f0d01c6d178194b5de1869fd1e5a90f180ee2cc
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-09-29 14:16:01 -0400

    feat(SCHEDULER): streamline logging, refactor

--- lib/backup/job/tests/job/test_backup_job.sh
+++ lib/backup/job/tests/job/test_backup_job.sh
@@ -16,6 +16,7 @@ setup_suite() {
     RPI_BACKUP_JOB_REMOTE_PARAMETER
     RPI_BACKUP_JOB_QUEUE
   )
+
 }
 
 setup() {
@@ -25,7 +26,6 @@ setup() {
   _mock.create _backup_job_cli
 
   _backup_job_args.mock.set.subcommand "_fixture_load_backup_job_parameters"
-  _backup_job_task_event_wrapper.mock.set.keywords "RPI_BACKUP_JOB_QUEUE"
 }
 
 _fixture_load_backup_job_parameters() {
@@ -44,9 +44,9 @@ _fixture_load_backup_job_parameters() {
   @parametrize \
     "${1}" \
     "TEST_COMMAND_ARGS_DEFINITION" \
-    "0_arguments______;;" \
-    "1_argument_______;arg1" \
-    "2_arguments______;arg1|arg2"
+    "0_arguments;;" \
+    "1_argument_;arg1" \
+    "2_arguments;arg1|arg2"
 }
 
 @parametrize_with_jobs() {
@@ -59,8 +59,9 @@ _fixture_load_backup_job_parameters() {
     "scenario2;job_name2;job_group2;local_source2;750;rsync_folder2;700;tarball_folder2;600;tarball_versions2;key_path2;remote_target2;remote_param2;job_queue2"
 }
 
-# shellcheck disable=SC2034
-test_backup_job__@vary__@vary__passes_all_received_arguments_to_backup_args() {
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_ok____@vary__@vary__passes_all_received_arguments_to_backup_args() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[0]}"
   local command_args=()
 
   stdlib.array.make.from_string command_args "|" "${TEST_COMMAND_ARGS_DEFINITION}"
@@ -72,64 +73,13 @@ test_backup_job__@vary__@vary__passes_all_received_arguments_to_backup_args() {
 }
 
 @parametrize.compose \
-  test_backup_job__@vary__@vary__passes_all_received_arguments_to_backup_args \
+  test_backup_job__job_status_ok____@vary__@vary__passes_all_received_arguments_to_backup_args \
   @parametrize_with_jobs \
   @parametrize_with_args
 
-# shellcheck disable=SC2034
-test_backup_job__@vary__failure_event______logs_correct_messages() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="${TEST_BACKUP_JOB_QUEUE}"
-
-  _backup_job
-
-  _cli_log_notice.mock.assert_called_once_with \
-    "1( -- BACKUP JOB: Executing '${TEST_BACKUP_JOB_QUEUE}' task for job '${TEST_BACKUP_JOB_NAME}' ...)"
-}
-
-@parametrize_with_jobs \
-  test_backup_job__@vary__failure_event______logs_correct_messages
-
-# shellcheck disable=SC2034
-test_backup_job__@vary__failure_event______calls_error_event_wrapper() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="${TEST_BACKUP_JOB_QUEUE}"
-  local RPI_BACKUP_JOB_FAILURE_QUEUE="MOCKED_FAILURE_QUEUE_NAME"
-
-  _backup_job
-
-  _backup_job_task_event_wrapper.mock.assert_called_once_with \
-    "1(event-backup-job-task-error.sh) RPI_BACKUP_JOB_QUEUE(${RPI_BACKUP_JOB_FAILURE_QUEUE})"
-}
-
-@parametrize_with_jobs \
-  test_backup_job__@vary__failure_event______calls_error_event_wrapper
-
-# shellcheck disable=SC2034
-test_backup_job__@vary__failure_event______does_not_call_backup_cli() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="${TEST_BACKUP_JOB_QUEUE}"
-
-  _backup_job
-
-  _backup_job_cli.mock.assert_not_called
-}
-
-@parametrize_with_jobs \
-  test_backup_job__@vary__failure_event______does_not_call_backup_cli
-
-# shellcheck disable=SC2034
-test_backup_job__@vary__failure_event______returns_0() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="${TEST_BACKUP_JOB_QUEUE}"
-
-  _capture.rc _backup_job
-
-  assert_rc 0
-}
-
-@parametrize_with_jobs \
-  test_backup_job__@vary__failure_event______returns_0
-
-# shellcheck disable=SC2034
-test_backup_job__@vary__non_failure_event__logs_correct_messages() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="MOCKED_NON_MATCHING_QUEUE_NAME"
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_ok____@vary_______________logs_correct_messages() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[0]}"
 
   _backup_job
 
@@ -139,11 +89,11 @@ test_backup_job__@vary__non_failure_event__logs_correct_messages() {
 }
 
 @parametrize_with_jobs \
-  test_backup_job__@vary__non_failure_event__logs_correct_messages
+  test_backup_job__job_status_ok____@vary_______________logs_correct_messages
 
-# shellcheck disable=SC2034
-test_backup_job__@vary__non_failure_event__calls_backup_cli_with_correct_queue() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="MOCKED_NON_MATCHING_QUEUE_NAME"
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_ok____@vary_______________calls_backup_cli_with_correct_queue() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[0]}"
 
   _backup_job
 
@@ -151,11 +101,11 @@ test_backup_job__@vary__non_failure_event__calls_backup_cli_with_correct_queue()
 }
 
 @parametrize_with_jobs \
-  test_backup_job__@vary__non_failure_event__calls_backup_cli_with_correct_queue
+  test_backup_job__job_status_ok____@vary_______________calls_backup_cli_with_correct_queue
 
-# shellcheck disable=SC2034
-test_backup_job__@vary__non_failure_event__does_not_call_error_event_wrapper() {
-  local RPI_BACKUP_QUEUE_FAILED_TASK_EVENT="MOCKED_NON_MATCHING_QUEUE_NAME"
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_ok____@vary_______________does_not_call_error_event_wrapper() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[0]}"
 
   _backup_job
 
@@ -163,4 +113,54 @@ test_backup_job__@vary__non_failure_event__does_not_call_error_event_wrapper() {
 }
 
 @parametrize_with_jobs \
-  test_backup_job__@vary__non_failure_event__does_not_call_error_event_wrapper
+  test_backup_job__job_status_ok____@vary_______________does_not_call_error_event_wrapper
+
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_fail__@vary_______________logs_correct_messages() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[1]}"
+
+  _backup_job
+
+  _cli_log_notice.mock.assert_called_once_with \
+    "1( -- BACKUP JOB: Executing '${TEST_BACKUP_JOB_QUEUE}' task for job '${TEST_BACKUP_JOB_NAME}' ...)"
+}
+
+@parametrize_with_jobs \
+  test_backup_job__job_status_fail__@vary_______________logs_correct_messages
+
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_fail__@vary_______________calls_error_event_wrapper() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[1]}"
+
+  _backup_job
+
+  _backup_job_task_event_wrapper.mock.assert_called_once_with \
+    "1(event-backup-job-task-error.sh)"
+}
+
+@parametrize_with_jobs \
+  test_backup_job__job_status_fail__@vary_______________calls_error_event_wrapper
+
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_fail__@vary_______________does_not_call_backup_cli() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[1]}"
+
+  _backup_job
+
+  _backup_job_cli.mock.assert_not_called
+}
+
+@parametrize_with_jobs \
+  test_backup_job__job_status_fail__@vary_______________does_not_call_backup_cli
+
+# shellcheck disable=SC2034,SC2153
+test_backup_job__job_status_fail__@vary_______________returns_0() {
+  local RPI_BACKUP_JOB_STATUS="${RPI_BACKUP_JOB_STATUSES[1]}"
+
+  _capture.rc _backup_job
+
+  assert_rc 0
+}
+
+@parametrize_with_jobs \
+  test_backup_job__job_status_fail__@vary_______________returns_0
