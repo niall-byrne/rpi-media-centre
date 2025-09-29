a3d3ef59199b0bbc261a18f2f8b83a8e661fa97b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-09-29 14:16:01 -0400

    feat(SCHEDULER): streamline logging, refactor

--- lib/backup/tests/scheduler/test_backup_scheduler_job_run.sh
+++ lib/backup/tests/scheduler/test_backup_scheduler_job_run.sh
@@ -6,7 +6,7 @@ setup() {
   _mock.create basename
   _mock.create mock_job
   _mock.create _backup_scheduler_job_promote
-  _mock.create _event_script
+  _mock.create _backup_scheduler_job_error
 
   TEST_QUEUE1_NAME="QUEUE1"
 }
@@ -47,14 +47,17 @@ test_backup_scheduler_job_run__@vary__@vary__calls_basename_to_identify_job() {
   @parametrize_with_successful_job \
   @parametrize_with_unsuccessful_job
 
-test_backup_scheduler_job_run__@vary__starts_the_job() {
+test_backup_scheduler_job_run__@vary__starts_the_job_with_status_ok() {
+  mock_job.mock.set.keywords "RPI_BACKUP_JOB_STATUS"
+
   _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
 
-  mock_job.mock.assert_called_once_with "1(${TEST_QUEUE1_NAME})"
+  mock_job.mock.assert_called_once_with \
+    "1(${TEST_QUEUE1_NAME}) RPI_BACKUP_JOB_STATUS(${RPI_BACKUP_JOB_STATUSES[0]})"
 }
 
 @parametrize_with_successful_job \
-  test_backup_scheduler_job_run__@vary__starts_the_job
+  test_backup_scheduler_job_run__@vary__starts_the_job_with_status_ok
 
 test_backup_scheduler_job_run__@vary__returns_status_code_0() {
   _capture.rc _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
@@ -88,63 +91,30 @@ test_backup_scheduler_job_run__@vary__promotes_job() {
 @parametrize_with_successful_job \
   test_backup_scheduler_job_run__@vary__promotes_job
 
-test_backup_scheduler_job_run__@vary__logs_expected_error_messages() {
+test_backup_scheduler_job_run__@vary__does_not_log_success_message() {
   _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
 
-  _cli_log_notice.mock.assert_called_once_with \
-    "1(BACKUP SCHEDULER: Backup job '${TEST_JOB_NAME}' - is starting the '${TEST_QUEUE1_NAME}' task ...)"
-  _cli_log_error.mock.assert_calls_are \
-    "1(BACKUP SCHEDULER: Backup job '${TEST_JOB_NAME}' - has failed the '${TEST_QUEUE1_NAME}' task !)" \
-    "1(BACKUP SCHEDULER: This job will be retried tomorrow.)"
+  _cli_log_success.mock.assert_not_called
 }
 
 @parametrize_with_unsuccessful_job \
-  test_backup_scheduler_job_run__@vary__logs_expected_error_messages
+  test_backup_scheduler_job_run__@vary__does_not_log_success_message
 
-test_backup_scheduler_job_run__@vary__retries_the_job() {
+test_backup_scheduler_job_run__@vary__calls_scheduler_job_error() {
   _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
 
-  mock_job.mock.assert_count_is "2"
-  mock_job.mock.assert_call_n_is "1" "1(${TEST_QUEUE1_NAME})"
-  mock_job.mock.assert_call_n_is "2" "1(${RPI_BACKUP_QUEUE_FAILED_TASK_EVENT})"
+  _backup_scheduler_job_error.mock.assert_called_once_with \
+    "1(${TEST_QUEUE1_NAME}) 2(mock_job)"
 }
 
 @parametrize_with_unsuccessful_job \
-  test_backup_scheduler_job_run__@vary__retries_the_job
+  test_backup_scheduler_job_run__@vary__calls_scheduler_job_error
 
-test_backup_scheduler_job_run__job_fails_______retry_fails_____calls_event_script() {
-  basename.mock.set.stdout "mock_failed_job"
-  mock_job.mock.set.side_effects "return 1" "return 1"
-
-  _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
-
-  _event_script.mock.assert_called_once_with \
-    "1(event-backup-scheduler-error.sh)"
-}
-
-test_backup_scheduler_job_run__job_fails_______retry_fails_____returns_status_code_0() {
-  basename.mock.set.stdout "mock_failed_job"
-  mock_job.mock.set.side_effects "return 1" "return 1"
-
+test_backup_scheduler_job_run__@vary__still_returns_status_code_0() {
   _capture.rc _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
 
   assert_rc "0"
 }
 
-test_backup_scheduler_job_run__job_fails_______retry_succeeds__does_not_call_event_script() {
-  basename.mock.set.stdout "mock_failed_job"
-  mock_job.mock.set.side_effects "return 1" "return 0"
-
-  _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
-
-  _event_script.mock.assert_not_called
-}
-
-test_backup_scheduler_job_run__job_fails_______retry_succeeds__returns_status_code_0() {
-  basename.mock.set.stdout "mock_failed_job"
-  mock_job.mock.set.side_effects "return 1" "return 0"
-
-  _capture.rc _backup_scheduler_job_run "${TEST_QUEUE1_NAME}" mock_job
-
-  assert_rc "0"
-}
+@parametrize_with_unsuccessful_job \
+  test_backup_scheduler_job_run__@vary__still_returns_status_code_0
